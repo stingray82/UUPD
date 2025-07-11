9170c03fefb657f7e7e48c31b2f3ddda38471349
Stingray82 <11146912+stingray82@users.noreply.github.com>
2025-07-11 10:13:22 +0100

    Fix Warning in PHP 8.1 +

--- updater.php
+++ updater.php
@@ -169,7 +169,7 @@ if ( ! class_exists( __NAMESPACE__ . '\UUPD_Updater_V1' ) ) {
         private function fetch_remote() {
             $c    = $this->config;
             $slug = rawurlencode( $c['slug'] );
-            $key  = rawurlencode( $c['key'] ?? '' );
+            $key = rawurlencode( isset( $c['key'] ) ? $c['key'] : '' );
             $host = rawurlencode( wp_parse_url( untrailingslashit( home_url() ), PHP_URL_HOST ) );
             $separator = strpos( $c['server'], '?' ) === false ? '?' : '&';
             $url = ( str_ends_with( $c['server'], '.json' ) ? $c['server'] : untrailingslashit( $c['server'] ) )
